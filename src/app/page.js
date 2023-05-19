7521dbe460b4cbc260567b73e411a099829b8c23
xBlackEaglex <roberto.smusic.1@gmail.com>
2023-05-18 19:48:27 -0600

    se agrego la anulacion del boton de retroceso

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,25 @@
+"use client"
+
 import Container from "./components/Container"
 import Header from "./components/Header"
 import styles from "../styles/main.module.css"
 import { UseNumMesaProvider } from "./providers/numMesaContext"
+import { useEffect } from "react"
 
 export default function App() {
+
+  useEffect(() => {
+    const disableBackButton = () => {
+      window.history.pushState(null, document.title, window.location.href);
+    };
+
+    window.addEventListener('popstate', disableBackButton);
+
+    return () => {
+      window.removeEventListener('popstate', disableBackButton);
+    };
+  }, []);
+
   return (
     <div className={styles.main}>
       <UseNumMesaProvider>

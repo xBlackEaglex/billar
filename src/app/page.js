18e42f1a4a7886baca11660373c4d139397afe7c
xBlackEaglex <roberto.smusic.1@gmail.com>
2023-05-18 20:21:41 -0600

    se corrigio el uso del boton de retroceso y elimino los import inecesarios

--- src/app/page.js
+++ src/app/page.js
@@ -4,11 +4,9 @@ import Container from "./components/Container";
 import Header from "./components/Header";
 import styles from "../styles/main.module.css";
 import { UseNumMesaProvider } from "./providers/numMesaContext";
-import { useEffect } from "react";
-import { useRouter } from "next/router";
 
 export default function App() {
-  
+
     var bPreguntar = true;
 
     window.onbeforeunload = preguntarAntesDeSalir;
